bcc32639ce444a6ea7e7f2be75ca66787220639b
shinyZu <shinysirichandra1141@gmail.com>
2022-10-23 22:50:10 +0530

    Updated Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo{
     public static void main(String[] args) {
-        String str = "Hello World";
+        String str = "Hello Java!";
         char[] chars = str.toCharArray();
         for (int i = 0; i < chars.length; i++) {
 		System.out.println(chars[i]);
