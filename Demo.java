f970e0c284f09eaee646f4a51bc9a97e066c9ab3
shinyZu <shinysirichandra1141@gmail.com>
2022-10-23 23:03:19 +0530

    Updated Content of  Demo.java

--- Demo.java
+++ Demo.java
@@ -1,9 +1,12 @@
 public class Demo{
     public static void main(String[] args) {
-        String str = "Hello Java!";
+        String str = "Hello World", str2="";
         char[] chars = str.toCharArray();
         for (int i = 0; i < chars.length; i++) {
-		System.out.println(chars[i]);
+            if (!Character.isWhitespace(chars[i])) {
+                str2 = str2 + chars[i];
+            }
         }
+        System.out.println(str2);
     }
 }
